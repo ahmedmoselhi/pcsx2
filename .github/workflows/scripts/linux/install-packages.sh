9053080246541bbf42469e84771e125aaf412a01
987123879113 <63495610+987123879113@users.noreply.github.com>
2021-12-09 15:25:34 +0900

    Use the right libusb package

--- .github/workflows/scripts/linux/install-packages.sh
+++ .github/workflows/scripts/linux/install-packages.sh
@@ -27,7 +27,7 @@ declare -a PCSX2_PACKAGES=(
   "libsdl2-dev"
   "libsamplerate0-dev"
   "libsoundtouch-dev"
-  "libusb-dev"
+  "libusb-1.0-0-dev"
   "libwxgtk3.0-gtk3-dev"
   "libx11-xcb-dev"
   "libxml2-dev"
